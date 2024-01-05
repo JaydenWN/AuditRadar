a47395317918c152b105b3a65759e5528568c27c
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2024-01-05 16:20:36 +1100

    basic layout with mantine AppShell

---
+++ frontend/app/components/ui/Navbar.jsx
@@ -0,0 +1,9 @@
+import { AppShell } from "@mantine/core"
+
+export default function Navbar(){
+    return(
+        <AppShell.Navbar p='md'>
+            Navigation
+        </AppShell.Navbar>
+    )
+}
--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,11 +9,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import '@mantine/core/styles.css'
+import { ColorSchemeScript, MantineProvider, AppShell, Burger, Group, Title, Stack } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import Navbar from './components/ui/Navbar'
+import { FaMagnifyingGlass } from "react-icons/fa6/index.js";
+
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <html lang="en">
       <head>
@@ -21,12 +30,49 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <ColorSchemeScript />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <MantineProvider>
+
+          <AppShell
+            header={{ height: {base : 60, md: 80} }}
+            navbar={{
+              width: 200,
+              breakpoint: 'sm',
+              collapsed: { mobile: !opened },
+            }}
+            padding="md"
+          >
+            <AppShell.Header>
+              <Stack h='100%' justify="center" p='lg'>
+                <Group justify="space-between"> 
+                  <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                  />
+                  <Group justify="space-between" gap='xs'>
+                    <Title order={2}><FaMagnifyingGlass /></Title>
+                    <Title>Audit Radar</Title>
+                  </Group>
+                </Group>
+              </Stack>
+            </AppShell.Header>
+            
+            <Navbar/>
+
+            <AppShell.Main >
+              <Outlet />
+            </AppShell.Main>
+
+          </AppShell>
+
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </MantineProvider>
       </body>
     </html>
   );
