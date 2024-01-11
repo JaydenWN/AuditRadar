98b413671a8ba5ab738ff430880ac3b7db867642
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2024-01-12 10:43:48 +1100

    used useDisclosure mantine hook to close navigation menu on user click

--- frontend/app/components/ui/Navbar.jsx
+++ frontend/app/components/ui/Navbar.jsx
@@ -1,4 +1,5 @@
-import { AppShell, Box} from "@mantine/core"
+import { AppShell} from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks";
 import { Link } from "@remix-run/react";
 import { NavLink as MantineNavLink } from "@mantine/core";
 import navbarLinks from '../Navbar-Links'
@@ -7,10 +8,14 @@ import { useLocation } from "@remix-run/react";
 import NewSpaceButton from "./New_Space_Button";
 
 export default function Navbar(){
-
+    const [handlers] = useDisclosure();
     const location = useLocation()
     console.log(location.pathname)
 
+    function handleNavClick(index){
+        setActive(index)
+        handlers.close()
+    }
 //https://mantine.dev/core/nav-link
     const [active, setActive] = useState(0);
 
@@ -26,7 +31,7 @@ export default function Navbar(){
         label={item.label}
         description={item.description}
         leftSection={item.leftSection}
-        onClick={() => setActive(index)}
+        onClick={() => handleNavClick(index)}
         >
         {item.children ? item.children.map((itemChild)=>(
             <MantineNavLink
