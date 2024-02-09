e97c8c56d9a8116f22efcce310c9a39e7d002da9
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2024-02-09 15:23:20 +1100

    image object now deleted from s3 when user deletes finding

--- frontend/app/routes/findings.edit.jsx
+++ frontend/app/routes/findings.edit.jsx
@@ -2,6 +2,7 @@ import { redirect } from "@remix-run/react"
 import { prisma } from "../utils/db.server"
 import getUser from "../utils/getUser"
 import { requireUserId } from "../utils/session.server"
+import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3"
 
 export async function action({request}){
     const data = await request.formData()
@@ -61,6 +62,31 @@ export async function action({request}){
         }
     }
     if(request.method === "DELETE"){
+
+        const currentFinding = await prisma.finding.findFirst({
+            where : {
+                id : Number(data.get('id')),
+                userId : user.id
+            }
+        })
+
+        const key = currentFinding.image.split('/')[3]
+        const deleteCommand = new DeleteObjectCommand({Bucket : process.env.BUCKET, Key: key})
+
+        const s3client = new S3Client({
+            region : process.env.REGION,
+            credentials: {
+                secretAccessKey : process.env.S3_SECRET,
+                accessKeyId : process.env.S3_ACCESS
+            }
+        })
+
+        try{
+            await s3client.send(deleteCommand)
+        }catch (e){
+            console.log(`Error Deleting S3 Bucket Object : ${e}`)
+        }
+
         return await prisma.finding.delete({
             where : {
                 id : Number(data.get('id')),
